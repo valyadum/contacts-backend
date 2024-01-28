5549bf5d37f553f72508e5d7b6ce4fb465176942
Valentyna Dumbrava <valyamoldovan55555@outlook.com>
2024-01-28 20:08:44 +0200

    summary

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-// import mongoose from "mongoose";
 import contactsRouter from "./routes/contactsRouter.js";
-
-// const DB_HOST ="mongodb+srv://valyadum:10122010Dum@cluster0.0q9ikbl.mongodb.net/db-contacts?retryWrites=true&w=majority"
-// mongoose.connect(DB_HOST)
-//   .then(() => {
-//     app.listen(3000);
-//     console.log("Database connection successful");
-//   })
-//   .catch((err) => {
-//     console.log(err.message);
-//     process.exit(1);
-
-//   })
-
-
+import 'dotenv/config';
 
 export const app = express();
 
@@ -34,21 +20,3 @@ app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
-
-// app.listen(3000, () => {
-//   console.log("Server is running. Use our API on port: 3000");
-// });
-
-// *****************
-
-// const express = require('express');
-// const router = express.Router();
-// const ctrlTask = require('../controller');
-
-// router.get('/tasks', ctrlTask.get);
-// router.get('/tasks/:id', ctrlTask.getById);
-// router.post('/tasks', ctrlTask.create);
-// router.put('/tasks/:id', ctrlTask.update);
-// router.patch('/tasks/:id/status', ctrlTask.updateStatus);
-// router.delete('/tasks/:id', ctrlTask.remove);
-// module.exports = router;
--- server.js
+++ server.js
@@ -1,63 +1,9 @@
-// const express = require('express');
-// const cors = require('cors');
-// const mongoose = require('mongoose');
-// require('dotenv').config();
-// const app = express();
-
-// // parse application/json
-// app.use(express.json());
-// // cors
-// app.use(cors());
-// const routerApi = require('./api');
-// app.use('/api', routerApi);
-// app.use((_, res, __) => {
-//     res.status(404).json({
-//         status: 'error',
-//         code: 404,
-//         message: 'Use api on routes: /api/tasks',
-//         data: 'Not found',
-//     });
-// });
-// app.use((err, _, res, __) => {
-//     console.log(err.stack);
-//     res.status(500).json({
-//         status: 'fail',
-//         code: 500,
-//         message: err.message,
-//         data: 'Internal Server Error',
-//     });
-// });
-// const PORT = process.env.PORT || 3000;
-// const uriDb = process.env.DB_HOST;
-// const connection = mongoose.connect(uriDb, {
-//     promiseLibrary: global.Promise,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-// });
-// connection
-//     .then(() => {
-//         app.listen(PORT, function () {
-//             console.log(Server running.Use our API on port: ${ PORT });
-//         });
-//     })
-//     .catch(err =>
-//         console.log(Server not running.Error message: ${ err.message }),
-//     );
-
-
-
-
-import express from "express";
 import mongoose from "mongoose";
 import { app } from "./app.js";
 import 'dotenv/config';
-// import dotenv from "dotenv";
-// dotenv.config()
-// import { DB_HOST } from "./config.js";
-//process.env.DB_HOST - місце знаходження ключа на сервері
-//  const DB_HOST = "mongodb+srv://valyadum:10122010Dum@cluster0.0q9ikbl.mongodb.net/db-contacts?retryWrites=true&w=majority"
-const { DB_HOST, PORT=3000 } = process.env;
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
 mongoose.connect(DB_HOST)
     .then(() => {
         app.listen(PORT);
